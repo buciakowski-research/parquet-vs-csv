322424aa4602c48cfbb0fe9a1f5ff7cc25e9b746
buciakowski-research <167675442+buciakowski-research@users.noreply.github.com>
2024-04-21 20:27:41 +0200

    Update test_read_data.r

--- test_read_data.r
+++ test_read_data.r
@@ -1,9 +1,10 @@
 library(tictoc)
 library(arrow)
+library(tidyverse)
 
 # Read csv file using pandas and get time of read file in [s]
 start_time = tic()
-df_1 <- read.csv("sample.csv")
+df_1 <- read_csv("sample.csv")
 stop_time = toc()
 print(paste('The csv read took ',gsub(" sec elapsed","",stop_time$callback_msg),'seconds'))
 
@@ -12,5 +13,3 @@ start_time = tic()
 df_2 <- read_parquet("sample.parquet")
 stop_time = toc()
 print(paste('The parquet read took ',gsub(" sec elapsed","",stop_time$callback_msg),'seconds'))
-
-
